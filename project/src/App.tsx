25d4f268343886b099d9089db21e26cb51258b71
mutf1710-svg <nurupharma865@gmail.com>
2026-09-06 21:56:57 +0200

    pages amployment

---
+++ project/src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    window.location.replace(`${import.meta.env.BASE_URL}app.html`);
+  }, []);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <p className="text-gray-500">Redirection vers PharmaSys...</p>
+    </div>
+  );
+}
+
+export default App;
